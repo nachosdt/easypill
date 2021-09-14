e0170e9670d249a4f65274ed147103e05c38de06
fpicong71 <fpicong@yahoo.es>
2021-09-14 17:13:27 +0200

    Relanzar master(Kika)

--- src/app/pages/prospecto/prospecto.page.ts
+++ src/app/pages/prospecto/prospecto.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-prospecto',
   templateUrl: './prospecto.page.html',
   styleUrls: ['./prospecto.page.scss'],
 })
+
 export class ProspectoPage implements OnInit {
+
+  public busqueda:string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buscar(form:NgForm){
+    
   }
 
 }

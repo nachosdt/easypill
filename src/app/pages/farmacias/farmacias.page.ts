b5c0c6737de2900397b7da375c352816b8f9fe7c
Nacho978 <elespiritudelacontradiccion@gmail.com>
2021-09-15 16:48:26 +0200

    Farmacias Page & Mapa (Nacho)

--- src/app/pages/farmacias/farmacias.page.ts
+++ src/app/pages/farmacias/farmacias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-farmacias',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FarmaciasPage implements OnInit {
 
-  constructor() { }
+  map: Leaflet.Map;
+  constructor() {}  
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    this.map = Leaflet.map('mapId',{ zoomControl: false }).setView([40.4, -3.7], 10);
+    Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: '',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoibmFjaG83OCIsImEiOiJja3RibjR5NGowbTQxMndteWtvNTMwMGl1In0.SyZ7hjPO3837MJmMAWbNzw'
+  }).addTo(this.map);
+
+    // Leaflet.marker([28.6, 77]).addTo(this.map).bindPopup('Delhi').openPopup();
+    // Leaflet.marker([34, 77]).addTo(this.map).bindPopup('Leh').openPopup();    
+  }
 
   ngOnInit() {
   }

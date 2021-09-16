19b9bed450707c48d132fe45bacc97078ae5cf6b
Nacho978 <elespiritudelacontradiccion@gmail.com>
2021-09-16 10:33:51 +0200

    Mapa centrado en la posición del usuario (Nacho)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ComponentsModule } from './components/components.module';
 
+/* Geolocation para localizar la posición del usuario */
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ComponentsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Geolocation], // Añadido Geolocation a providers
   bootstrap: [AppComponent],
 })
 export class AppModule { }

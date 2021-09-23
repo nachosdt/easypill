2b135e330a0b0a9503e0d1582d8bfd2412a1b896
Martorell93 <jaime.martorell.lopez@gmail.com>
2021-09-23 17:10:28 +0200

    arreglo contactos page (Jaime)

--- src/app/shared/contactos.service.ts
+++ src/app/shared/contactos.service.ts
@@ -41,50 +41,52 @@ export class ContactosService {
 
   // LLAMADA A LA API GET CONTACTOS
   async getContacto() {
-    try {
-      let id: number = this.servicioGeneral.idUsuario;
-      let url: string = `http://localhost:4000/contacto?id=7`;
+    if (this.contactos = []) {
+      try {
+        let id: number = this.servicioGeneral.idUsuario;
+        let url: string = `http://localhost:4000/contacto?id=7`;
 
-      let param: any =
-      {
-          headers: {"Content-type": "application/json; chasert= UTF-8"},
-          method: "GET"
-      }
+        let param: any =
+        {
+            headers: {"Content-type": "application/json; chasert= UTF-8"},
+            method: "GET"
+        }
 
-      let data = await fetch(url, param);
-      let result = await data.json();
+        let data = await fetch(url, param);
+        let result = await data.json();
 
-      for (let i in result.datos) {
-        let newContact: Contacto = new Contacto ();
-        newContact.nombreContacto = result.datos[i].alias;
-        newContact.tlfContacto = result.datos[i].telefono;
-        newContact.emailContacto = result.datos[i].email;
-        newContact.posicionArray = parseFloat(i);
-        newContact.idContacto = result.datos[i].id_contacto;
-        newContact.idUsuario = id;
-        if (result.datos[i].notificacion == "ambos") {
-          newContact.notifEmail = true;
-          newContact.notifSms = true;
-        }
-        else if (result.datos[i].notificacion == "email") {
-          newContact.notifEmail = true;
-          newContact.notifSms = false;
-        }
-        else if (result.datos[i].notificacion == "sms") {
-          newContact.notifEmail = false;
-          newContact.notifSms = true;
-        }
-        else {
-            newContact.notifEmail = false;
+        for (let i in result.datos) {
+          let newContact: Contacto = new Contacto ();
+          newContact.nombreContacto = result.datos[i].alias;
+          newContact.tlfContacto = result.datos[i].telefono;
+          newContact.emailContacto = result.datos[i].email;
+          newContact.posicionArray = parseFloat(i);
+          newContact.idContacto = result.datos[i].id_contacto;
+          newContact.idUsuario = id;
+          if (result.datos[i].notificacion == "ambos") {
+            newContact.notifEmail = true;
+            newContact.notifSms = true;
+          }
+          else if (result.datos[i].notificacion == "email") {
+            newContact.notifEmail = true;
             newContact.notifSms = false;
+          }
+          else if (result.datos[i].notificacion == "sms") {
+            newContact.notifEmail = false;
+            newContact.notifSms = true;
+          }
+          else {
+              newContact.notifEmail = false;
+              newContact.notifSms = false;
+          }
+          this.contactos.push(newContact);
         }
-        this.contactos.push(newContact);
+        return this.contactos;
       }
-      return this.contactos;
-    }
-    catch(error) 
-    {
-        console.log(error);
+      catch(error) 
+      {
+          console.log(error);
+      }
     }
   }
 

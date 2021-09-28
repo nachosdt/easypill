fd07ed8d678af72231f05dc7c449f3cf24d49a3c
Nacho978 <elespiritudelacontradiccion@gmail.com>
2021-09-28 12:15:48 +0200

    Fix deploy 6

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/easypill"));
+app.use(express.static("www"));
 app.get("/*",function(request,response) {
-    response.sendFile("index.html",{root: __dirname + "/dist/easypill/"})
+    response.sendFile("index.html",{root: "www"})
 });
 
 app.listen(process.env.PORT || 8080);
